29bede2c7825524b34e6ca24c7fd0b6976fef56f
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-03-07 01:50:21 +0900

    update basepath

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
-        path: '',
+        path: '/calculator-vuetify/',
         name: 'Home',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
