e74edeb9373327cfb56c7e04d1402a7ccaf8c4bb
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-03-09 21:38:04 +0900

    [feat] ダークテーマ切り替えスイッチ実装

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -5,9 +5,29 @@
 
       Simple Calculator
     </v-app-bar-title>
+
+    <!-- dark theme switch -->
+    <template v-slot:append>
+      <v-switch
+        v-model="darkTheme"
+        @update:model-value="changeTheme"
+        :prepend-icon="darkTheme ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+        hide-details
+        inset
+        class="mr-auto"
+      />
+    </template>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+import { ref } from 'vue'
+import { useTheme } from 'vuetify'
+
+const darkTheme = ref(false)
+const theme = useTheme()
+
+const changeTheme = () => {
+  theme.global.name.value = darkTheme.value ? 'dark' : 'light'
+}
 </script>
